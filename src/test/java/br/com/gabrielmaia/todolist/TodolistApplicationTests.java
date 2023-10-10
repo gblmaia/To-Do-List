41f143147bf1bde917c8988c2bf8ab2fb566253c
Gabriel Alves <gbl.alves556@gmail.com>
2023-10-10 11:30:16 -0300

    first commit

---
+++ src/test/java/br/com/gabrielmaia/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabrielmaia.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
