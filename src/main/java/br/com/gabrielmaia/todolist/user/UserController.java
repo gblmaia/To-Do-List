a3d84a1347f9c63f961fb0d5a0e161515ee60179
Gabriel Alves <gbl.alves556@gmail.com>
2023-10-10 15:59:10 -0300

    Adição de response code para cadastro de usuário, usuário cadastrado e usuário já cadastrado

--- src/main/java/br/com/gabrielmaia/todolist/user/UserController.java
+++ src/main/java/br/com/gabrielmaia/todolist/user/UserController.java
@@ -1,5 +1,9 @@
 package br.com.gabrielmaia.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,12 +13,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping ("/users")
 public class UserController {
     
-    @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
+    @Autowired
+    private IUserRepository userRepository;
 
-        System.out.println(userModel.getUsername()); 
-        System.out.println(userModel.getPassword());
-        System.out.println(userModel.getUsername());
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if(user != null) {
+            System.out.println("Usuário já existente");
+
+            //** Mensagem de erro */
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        } 
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 
 }
